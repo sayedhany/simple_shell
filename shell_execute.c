a46ff8e1c05b7e5ec7c4123267d6e353bf1e47e6
sayedhany <sayedh@mail.ru>
2023-04-22 16:55:54 -0700

    edit

--- shell_execute.c
+++ shell_execute.c
@@ -1,53 +1,31 @@
 #include "main.h"
 /**
- * execute_command - Executes the command given as input.
- * @args: An array of strings containing the command and its arguments.
- * Return: 1 on success, -1 on failure.
+ * execute_command - execute command with arguments
+ * @args: arguments for command
  */
-int execute_command(char **args)
+void execute_command(char **args)
 {
 	pid_t pid;
-	char *path = NULL;
-	struct stat st;
-
-	if (args == NULL || args[0] == NULL)
-		return (-1);
-
-	path = _getpath(args[0]);
-
-	if (path == NULL)
-	{
-		print_error(args[0], "not found");
-		return (-1);
-	}
-
-	if (stat(path, &st) == -1)
-	{
-		free(path);
-		print_error(args[0], "not found");
-		return (-1);
-	}
+	int status;
 
 	pid = fork();
-
 	if (pid == -1)
 	{
-		free(path);
-		perror("Error");
-		return (-1);
+		perror("Fork faild");
+		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
-		if (execve(path, args, environ) == -1)
+		if (execve(args[0], args, NULL) == -1)
 		{
-			free(path);
-			perror("Error");
+			perror("./hsh");
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
-		wait(NULL);
-
-	free(path);
-	return (1);
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
