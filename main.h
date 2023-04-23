9073ecc03467d051510cf2368e608481a9c760a1
sayedhany <sayedh@mail.ru>
2023-04-22 17:04:37 -0700

    add code

--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+/* functions prototypes */
 void execute_command(char **args);
 void print_prompt(void);
 char **tokenize(char *command);
